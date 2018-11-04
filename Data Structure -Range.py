6dda93e75ccb58d93bc724e6b756e4cd0b768d6d
plutobot <sandyc0203@gmail.com>
2018-11-04 16:49:01 -0600

    Add files via upload

---
+++ Data Structure -Range.py
@@ -0,0 +1,47 @@
+
+# coding: utf-8
+
+# 
+# ## 2.1 Using range() in creating other sequence objects
+
+# In[1]:
+
+
+list(range(10))
+
+
+# In[2]:
+
+
+list(range(1,11))
+
+
+# In[3]:
+
+
+list(range(0,30,5))
+
+
+# In[4]:
+
+
+list(range(0,10,3))
+
+
+# In[5]:
+
+
+list(range(0,-10,-1))
+
+
+# In[6]:
+
+
+list(range(0))
+
+
+# In[7]:
+
+
+list(range(1,0))
+
